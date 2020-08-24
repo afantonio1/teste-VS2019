cab1c5c1f81421b50e1051ac503e5f66c735f6f6
Alessandro Ferreira Antônio <afantonio1@gmail.com>
2020-08-24 11:23:06 -0300

    Primeiro Commit

---
+++ Console-Teste/Console-Teste/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Console_Teste
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Usando GIT com VS2019");
+            Console.ReadKey(true);
+        }
+    }
+}
